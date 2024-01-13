25b24e7f98c3a8d7598a992526e663ab4ec5f739
neiilj13 <91741919+neiilj13@users.noreply.github.com>
2024-01-13 20:04:54 +0530

    Update Dog Breed Classification.py

--- pages/Dog Breed Classification.py
+++ pages/Dog Breed Classification.py
@@ -41,10 +41,8 @@ if img is not None:
 	label=res[0].probs.top5
 	conf=res[0].probs.top5conf
 	conf=conf.tolist()
-	st.write("hi")
 
 	col1,col2 = st.columns(2)
-	st.write("hi")
 	col1.subheader(res[0].names[label[0]] +' with '+ str(conf[0])+' Confidence')
 	col1.subheader(res[0].names[label[1]] +' with '+ str(conf[1])+' Confidence')
 	col1.subheader(res[0].names[label[2]] +' with '+ str(conf[2])+' Confidence')
